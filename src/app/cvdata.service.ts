de4a853352a53da76f8d6273421c0ae98fccb7a5
Marinho <marinho123>
2021-01-22 23:37:31 +0000

    teste node

--- src/app/cvdata.service.ts
+++ src/app/cvdata.service.ts
@@ -18,7 +18,7 @@ export class CVDataService {
   }
 
   getPDF(){
-    return this.http.get('https://cvserver.azurewebsites.net/users', { responseType: 'text' })
+    return this.http.get('/users', { responseType: 'text' })
       .toPromise().then(value => {
         return value;
     });
