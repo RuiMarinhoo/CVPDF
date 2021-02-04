f0fab2a3de58cbdc5764d0d0219b29f1da58859b
Marinho <marinho123>
2021-02-04 19:10:19 +0000

    createPDF acabado 90%

--- src/app/cvdata.service.ts
+++ src/app/cvdata.service.ts
@@ -8,6 +8,10 @@ export class CVDataService {
 
   allCvData: any;
 
+  urlServer = 'https://cvserverapp.herokuapp.com/generateCV';
+  // urlServer = 'http://localhost:3000/generateCV/render';
+  urlRender = '/render';
+
   constructor(private http: HttpClient) { }
 
   setData(data){
@@ -18,15 +22,14 @@ export class CVDataService {
   }
 
   renderPDF(m, d){
-    // return this.http.post('https://cvserverapp.herokuapp.com/generateCV/render', {modelo: m, data: d}, { responseType: 'text' })
-    return this.http.post('http://localhost:3000/generateCV/render', {modelo: m, data: d}, { responseType: 'text' })
+    return this.http.post(this.urlServer + this.urlRender, {modelo: m, data: d}, { responseType: 'text' })
       .toPromise().then(value => {
         return value;
       });
   }
 
   getPDF(m, d){
-    return this.http.post('http://localhost:3000/generateCV/', {modelo: m, data: d}, { responseType: 'text' })
+    return this.http.post(this.urlServer, {modelo: m, data: d}, { responseType: 'text' })
       .toPromise().then(value => {
         return value;
       });
